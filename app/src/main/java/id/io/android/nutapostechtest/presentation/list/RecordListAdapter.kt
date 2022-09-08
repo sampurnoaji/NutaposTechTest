5d2d4f7f3bb6013d8f3915f96ededf00b999310c
Sampurno Aji <man.sanji23@gmail.com>
2022-09-08 16:37:56 +0700

    calculate net sales

--- app/src/main/java/id/io/android/nutapostechtest/presentation/list/RecordListAdapter.kt
+++ app/src/main/java/id/io/android/nutapostechtest/presentation/list/RecordListAdapter.kt
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import id.io.android.nutapostechtest.databinding.ItemRecordBinding
 import id.io.android.nutapostechtest.domain.model.Record
+import java.util.Locale
+import kotlin.math.roundToLong
 
 class RecordListAdapter : ListAdapter<Record, RecordListAdapter.ContentViewHolder>(DIFF_CALLBACK) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentViewHolder {
@@ -26,8 +28,18 @@ class RecordListAdapter : ListAdapter<Record, RecordListAdapter.ContentViewHolde
                 tvId.text = record.uangMasukId
                 tvTerimaDari.text = record.terimaDari
                 tvKeterangan.text = record.keterangan
-                tvJumlah.text = record.jumlah.toString()
+                tvJumlah.text = record.jumlah.toRupiah()
                 tvNomor.text = record.nomor
+                tvNetSales.text = (record.jumlah / 1.1).roundToLong().toRupiah()
+            }
+        }
+
+        fun Long.toRupiah(): String {
+            return try {
+                val localeId = Locale("id", "ID")
+                "Rp " + String.format(localeId, "%,d", this)
+            } catch (e: Exception) {
+                "-"
             }
         }
     }
